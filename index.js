f6add6d791d6eab0fd499e90be80c785a84adf75
kanekiken1234 <ruthvikrokz@gmail.com>
2021-11-24 19:56:14 +0530

    sign in done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const app = express();
 const config = require('./config');
 const users = require('./routes/users');
+const user = require('./routes/user');
 const DbConnect = require('./database/dbConnection')
 
 const { app: { port } } = config;
@@ -16,6 +17,7 @@ app.use(compression());
 DbConnect();
 
 app.use('/api/users', users);
+app.use('/api/user', user);
 
 app.listen(port, function () {
     console.log(`Server started on port ${port}...`);
---
+++ routes/user.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const validate = require('../middlewares/validation');
+const UserTable = require('../database/models/Users')
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+
+const usersSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required()
+})
+
+router.get("/signin", validate(usersSchema), async (req, res) => {
+    const { email, password } = req.body;
+    const user = {
+        email: email,
+        password: password
+    }
+    const result = await UserTable.findOne(user)
+    console.log(result)
+    if (!result)
+        return res.status(400).send({ error: "Invalid email or password." });
+
+    const token = {
+        'email': email
+    }
+    const accessToken = jwt.sign(token, process.env.JWT_SIGN);
+    res.status(201).send(accessToken);
+});
+
+module.exports = router
